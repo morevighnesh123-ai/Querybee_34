b6e9b952b9cc4d1d98ddec40b5ec8494b45d8e1a
QueryBee Developer <morevighnesh123@gmail.com>
2025-12-29 22:06:39 +0530

    fix: reduce glass effect on settings dialog for better title readability

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -10,7 +10,7 @@ const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'fixed inset-0 z-[9998] bg-black/25 backdrop-blur-[2px]',
+      'fixed inset-0 z-[9998] bg-black/15 backdrop-blur-[1px]',
       className
     )}
     {...props}
@@ -24,7 +24,7 @@ const DialogContent = React.forwardRef(({ className, ...props }, ref) => (
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-1/2 top-1/2 z-[9999] w-[calc(100vw-24px)] max-w-[420px] -translate-x-1/2 -translate-y-1/2 rounded-[18px] border border-[hsl(var(--qb-border))] bg-[hsl(var(--qb-bg))] p-4 text-[hsl(var(--qb-fg))] shadow-[0_20px_60px_rgba(0,0,0,0.25)]',
+        'fixed left-1/2 top-1/2 z-[9999] w-[calc(100vw-24px)] max-w-[420px] -translate-x-1/2 -translate-y-1/2 rounded-[18px] border border-[hsl(var(--qb-border))] bg-[hsl(var(--qb-bg))]/95 p-4 text-[hsl(var(--qb-fg))] shadow-[0_20px_60px_rgba(0,0,0,0.25)]',
         className
       )}
       {...props}
